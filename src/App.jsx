c64395eb98a5b57942209acb0e49d853488154d2
Mateo Francisco Laria <mateolaria2021@gmail.com>
2023-11-17 14:21:36 -0300

    modificaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 // App.jsx
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/header/header';
-import DivProducto from './components/divProducto/DivProducto';
-import Footer from './components/footer/Footer';
+import Header from './components/header/Header.jsx';
+import DivProducto from './components/divProducto/DivProducto.jsx';
+import Footer from './components/footer/Footer.jsx';
 
 function App() {
   return (
